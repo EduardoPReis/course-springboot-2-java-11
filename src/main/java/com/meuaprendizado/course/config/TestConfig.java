244bf89cbf624894bbd1e3255da94f9ccf7e0c73
Eduardo Reis <reisprogramer@gmail.com>
2022-06-17 13:10:07 -0300

    Category

--- src/main/java/com/meuaprendizado/course/config/TestConfig.java
+++ src/main/java/com/meuaprendizado/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.meuaprendizado.course.entities.Category;
 import com.meuaprendizado.course.entities.Order;
 import com.meuaprendizado.course.entities.User;
 import com.meuaprendizado.course.entities.enums.OrderStatus;
+import com.meuaprendizado.course.repositories.CategoryRepository;
 import com.meuaprendizado.course.repositories.OrderRepository;
 import com.meuaprendizado.course.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepositoy;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+
+		categoryRepositoy.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
---
+++ src/main/java/com/meuaprendizado/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.meuaprendizado.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meuaprendizado.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
